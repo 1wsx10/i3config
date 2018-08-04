79439a257f095b4a251f1b90cebf65a1ac8ce9a1
1wsx10 <1wsx10@gmail.com>
2018-08-04 10:10:36 +0000

    Initial commit
    	new file:   .gitignore
    	new file:   brightness.sh
    		script to store and change the brightness using light
    	new file:   config
    		main config file
    	new file:   escape.sh
    		script to send the escape key to X after a delay
    	new file:   i3status.conf
    		config file for i3 status bar
    	new file:   touchpad_toggle.sh
    		script to store and toggle the touchpad state
    	new file:   volume.sh
    		script to adjust volume

---
+++ brightness.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#brightness=4
+
+C=$(sed -n 's/#brightness=//;2p' $0)
+N=$C
+
+if [[ $1 == "up" ]]; then
+	echo "up"
+	N=$(expr $C + 1)
+
+elif [[ $1 == "down" ]]; then
+	echo "down"
+	N=$(expr $C - 1)
+fi
+
+if [[ $N -gt 10 ]]; then
+	N=10
+elif [[ $N -lt 0 ]]; then
+	N=0
+fi
+
+sed -i "2s/\(#brightness=\).*/\1$N/" $0
+C=$(sed -n 's/#brightness=//;2p' $0)
+light -S $(expr $C \* $C)
+
+L=$(light -G)
+echo "Light: $L"
---
+++ volume.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#volume=3
+
+C=$(sed -n 's/#volume=//;2p' $0)
+N=$C
+
+# raise or lower value
+if [[ $1 == "up" ]]; then
+	echo "up"
+	N=$(expr $C + 1)
+
+elif [[ $1 == "down" ]]; then
+	echo "down"
+	N=$(expr $C - 1)
+
+elif [[ $1 == "mute" ]]; then
+	amixer -q set Master toggle
+fi
+
+
+# make sure it doesn't go out of bounds
+if [[ $N -gt 10 ]]; then
+	N=10
+elif [[ $N -lt 0 ]]; then
+	N=0
+fi
+
+sed -i "2s/\(#volume=\).*/\1$N/" $0
+C=$(sed -n 's/#volume=//;2p' $0)
+amixer -q set Master $(expr $C \* 10)%
+
