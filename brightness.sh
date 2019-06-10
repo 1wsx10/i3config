caa287541b5f92cdaf189ee28fdc210aa3100985
1wsx10 <1wsx10@gmail.com>
2019-06-10 12:23:05 +1000

    made quote for niceness

--- brightness.sh
+++ brightness.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #brightness=4
 
-C=$(sed -n 's/#brightness=//;2p' $0)
+C=$(sed -n 's/#brightness=//;2p' "$0")
 N=$C
 
 if [[ $1 == "up" ]]; then
 	echo "up"
-	N=$(expr $C + 1)
+	N=$(dc -e "$C 1 +p")
 
 elif [[ $1 == "down" ]]; then
 	echo "down"
-	N=$(expr $C - 1)
+	N=$(dc -e "$C 1 -p")
 fi
 
 if [[ $N -gt 10 ]]; then
@@ -19,9 +19,9 @@ elif [[ $N -lt 0 ]]; then
 	N=0
 fi
 
-sed -i "2s/\(#brightness=\).*/\1$N/" $0
-C=$(sed -n 's/#brightness=//;2p' $0)
-light -S $(expr $C \* $C)
+sed -i "2s/\(#brightness=\).*/\1$N/" "$0"
+C=$(sed -n 's/#brightness=//;2p' "$0")
+light -S "$(dc -e "$C"' d*p')"
 
 L=$(light -G)
 echo "Light: $L"
