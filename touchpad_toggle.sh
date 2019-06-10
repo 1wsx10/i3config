18be61fb4c3c9556c2ed704e8fdb5f1e282e7e61
1wsx10 <1wsx10@gmail.com>
2019-06-10 12:29:52 +1000

    checked

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -2,12 +2,12 @@
 #xinput enable
 
 if [[ $1 != "set" ]]; then
-	sed -i '2s/#xinput enable/#xinput TEMP/' $0
-	sed -i '2s/#xinput disable/#xinput enable/' $0
-	sed -i '2s/#xinput TEMP/#xinput disable/' $0
+	sed -i '2s/#xinput enable/#xinput TEMP/' "$0"
+	sed -i '2s/#xinput disable/#xinput enable/' "$0"
+	sed -i '2s/#xinput TEMP/#xinput disable/' "$0"
 fi
 
 
 #suppress normal output (printing everything), grab the second line, remove #, then print the second line
-synclient $(sed -n '2s/#//;2p' "$0")
-$(sed -n '2s/#//;2p' $0) 'SynPS/2 Synaptics TouchPad'
+synclient "$(sed -n '2s/#//;2p' "$0")"
+"$(sed -n '2s/#//;2p' "$0")" 'SynPS/2 Synaptics TouchPad'
